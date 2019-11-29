1819a7cd16878deb3dd937832dbadea087550d78
Stathis Stamoulis <sstamoulis@mosystems.biz>
2019-11-29 21:06:42 +0000

    Create local build script

--- build.sh
+++ build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -e
-version=10.0.3
-checksum=96c256b8f8b1136aab2cd693cc242eb6806be138
-for debug in "true" "false"; do
-    echo "Building Dolibarr version $version$([ "$debug" = "true" ] && echo "with debug enabled")"
-    docker build \
-        --build-arg debug="$debug" \
-        --build-arg version="$version" \
-        --build-arg checksum="$checksum" \
-        -t mosystems/dolibarr:${version%%.*}$([ "$debug" = "true" ] && echo -debug) \
-        -t mosystems/dolibarr:${version%.*}$([ "$debug" = "true" ] && echo -debug) \
-        -t mosystems/dolibarr:${version}$([ "$debug" = "true" ] && echo -debug) \
-        .
-done
+DOCKERFILE_PATH=Dockerfile
+DOCKER_REPO='mosystems/dolibarr'
+IMAGE_NAME="$DOCKER_REPO:latest"
+. ./hooks/build
+. ./hooks/push
